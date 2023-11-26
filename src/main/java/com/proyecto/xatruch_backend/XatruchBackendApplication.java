8a2fa93d2979fc1297f845091e6c40530945bd0e
Mike Winchester <aleminapavon30@gmail.com>
2023-11-25 23:17:50 -0600

    First commit

---
+++ src/main/java/com/proyecto/xatruch_backend/XatruchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.xatruch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XatruchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XatruchBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/xatruch_backend/XatruchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.xatruch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XatruchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
